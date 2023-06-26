2924e3fbd82c567ec86910868295d8d60785ad88
Oleh Zanevych <ozanevych@ancestry.com>
2023-06-27 00:48:20 +0300

    Fixed injection of Users

--- integration-tests/src/test/scala/org/lnu/timetable/integration/tests/FacultiesAndDepartmentsBasicTestsSimulation.scala
+++ integration-tests/src/test/scala/org/lnu/timetable/integration/tests/FacultiesAndDepartmentsBasicTestsSimulation.scala
@@ -12,6 +12,6 @@ class FacultiesAndDepartmentsBasicTestsSimulation extends Simulation {
   val users = scenario("Faculties and departments basic testing").exec(TestCases.testCases)
 
   setUp(
-    users.inject(rampUsers(10))
+    users.inject(atOnceUsers(100))
   ).protocols(httpProtocol)
 }
